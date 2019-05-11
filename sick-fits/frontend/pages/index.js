e9ec93599ca80fe0163570cd320e7e6e3c66bb54
Afrowave <jimmy@afroshok.com>
2019-05-11 23:00:47 +0300

    UPDATE to the latest Wes Bos Advanced React Repo. CREATE a new branch with all the changes up to the beginning of video 9.

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Home = props => (
+	<div>
+		<p>Hey!! You are at Home.</p>
+	</div>
+)
+	
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Sell = props => (
+	<div>
+		<p>Sell like Mad!!</p>
+		
+	</div>
+)
+	
+export default Sell;
