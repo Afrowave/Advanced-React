169a4a035f8f02c08d97dc47fdd2c48f45d62815
Jimmy Gitonga <jimmy@afroshok.com>
2019-06-19 19:49:29 +0300

    CREATE Pagination
    This commit builds the pagination of the items view, breaking it into a
    number of pages.
    
    However, once a new item is loaded, the cache becomes stale and as of
    this commit, there was no solution to deleting part of the cache. Fetching
    afresh the items afresh can solve this but defeats the purpose of having
    the cache. Deleting the cache removes everything, including what is in
    the cart.

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,10 +3,12 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
+import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
+	query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+		items(skip: $skip, first: $first, orderBy: createdAt_DESC) {
 			id
 			title
 			price
@@ -34,7 +36,10 @@ class Items extends Component {
 		return (
 			<div>
 				<p>Items!</p>
-				<Query query={ALL_ITEMS_QUERY}>
+				<Pagination page={this.props.page} />
+				<Query query={ALL_ITEMS_QUERY} variables={{
+					skip: this.props.page * perPage - perPage,
+				}}>
 					{({ data, error, loading }) => {
 						if (loading) return <p>Loading ...</p>;
 						if (error) return <p>Error: {error.message}</p>;
@@ -47,6 +52,7 @@ class Items extends Component {
 						);
 					}}
 				</Query>
+				<Pagination page={this.props.page} />
 			</div>
 		);
 	}
